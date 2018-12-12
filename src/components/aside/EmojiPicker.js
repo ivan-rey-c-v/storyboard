0446b45d040604f781eb8c7fa43f4566b5826e9a
ivan rey c.v <ivan.rey.c.v@gmail.com>
2018-12-12 22:58:51 +0800

    Change emoji picker default emoji and title

--- src/components/aside/EmojiPicker.js
+++ src/components/aside/EmojiPicker.js
@@ -4,7 +4,13 @@ import { Picker } from 'emoji-mart'
 import '../../emoji-mart.css'
 
 function EmojiPicker(props) {
-	return <Picker onClick={props.handleEmojiClick} />
+	return (
+		<Picker
+			onClick={props.handleEmojiClick}
+			emoji="point_up"
+			title="Pick your emoji..."
+		/>
+	)
 }
 
 export default React.memo(EmojiPicker)
